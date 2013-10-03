8ca1ed751bb5b73b41c5c2ef191fa4423fc7dabe
lethi17 <lethi17@ccm-2610-e3.evergreen.edu>
2013-10-03 16:59:57 -0700

    Lab1

---
+++ hw1.py
@@ -0,0 +1,60 @@
+# Name:Huong Le
+# Evergreen Login: lethi17
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math
+
+
+
+
+
+
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+a = 1
+b = -5.86
+c = 8.5408
+t= b*b
+z= t-4 * a * c
+s= math.sqrt(z)
+Answer= (-b+s)/ (2 * a)
+
+
+print Answer
+
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
