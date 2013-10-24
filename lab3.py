a7ece51f5ae56408b67a9650f92fe77c9ffa9dd4
lethi17 <lethi17@ccm-2610-e3.evergreen.edu>
2013-10-24 16:31:49 -0700

    Lab3

--- lab3.py
+++ lab3.py
@@ -1,5 +1,14 @@
+series = raw_input("asnswer ")
 n= 3
-series = "fibonacci"
-if series == "fibonacci":
-    print series
-    
+
+# series = "fibonacci" 
+
+def lab3():
+    if series == "fibonacci":
+        return sum(range(0, 3 * n + 1, 3)) 
+    elif series == 'sum':
+        return 1000
+    else:
+        return 0
+        
+print lab3() + 1
