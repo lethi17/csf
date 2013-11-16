15beaba94e4aad4c078b51626a83c53748fa83e1
lethi17 <Hunnie@Huongs-MacBook-Pro.local>
2013-11-15 16:52:22 -0800

    HW6

---
+++ hw6.py
@@ -0,0 +1,76 @@
+# Name: Huong       
+# Evergreen Login: Lethi17  
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 6
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+for vowels in 'Python Is A Great Programming Language!':
+    if vowels == 'constant':
+        break 
+    print 'number of vowels : ', vowels
+    
+    
+for constants in 'Python Is A Great Programming Language!':
+    if constants== 'constant':
+        break 
+    print 'number of constatns: ', constants
+
+
+    
+
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 7
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 7 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
